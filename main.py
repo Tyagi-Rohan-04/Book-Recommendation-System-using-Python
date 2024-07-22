8bf7a61b68281c5fc618ee5a2180f09f3fc2cc33
Rohan Tyagi <tyagirohan798@gmail.com>
2024-07-23 05:21:46 +0530

    Initial creation of the app

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+import requests
+from PIL import ImageTk, Image
+from io import BytesIO
+import urllib.parse
+
+root = Tk()
+root.title("Book Recommendation System")
+root.geometry("1250x700")
+root. config(bg="#111119")
+
+root.mainloop()
