8abed45d98671ff019cd6d10bc409f7f793671bd
Rohan Tyagi <tyagirohan798@gmail.com>
2024-07-23 17:39:53 +0530

    Final update of the app

--- main.py
+++ main.py
@@ -5,10 +5,94 @@ from PIL import ImageTk, Image
 from io import BytesIO
 import urllib.parse
 
+class Request:
+    def __init__(self, method, args):
+        self.args = args
+        self.method = method
+
+inc = 0
+def fetch_information(title, poster, date, rating):
+    global inc
+    inc = inc+1
+    text[f'a{inc}'].config(text=title)
+    if check_var.get():
+        text2[f'a{inc}{inc}'].config(text=date)
+    else:
+        text2[f'a{inc}{inc}'].config(text="")
+
+    if check_var2.get():
+        text3[f'a{inc}{inc}{inc}'].config(text=rating)
+    else:
+        text3[f'a{inc}{inc}{inc}'].config(text="")
+
+    response = requests.get(poster)
+    img_data = response.content
+    img = (Image.open(BytesIO(img_data)))
+    resized_image = img.resize((140, 200))
+    photo2 = ImageTk.PhotoImage(resized_image)
+    image[f'b{inc}'].config(image=photo2)
+    image[f'b{inc}'].image = photo2
+
+
+def search():
+    global inc
+    inc = 0
+    request = Request('GET', {'search':Search.get()})
+
+    if request.method == 'GET':
+        search=urllib.parse.quote(request.args.get('search',''))
+        url = f"https://www.googleapis.com/books/v1/volumes?q={search}&maxResults=5"
+        response = requests.get(url)
+        # print(response.json())
+
+        if response.status_code==200:           # 200 means the request was successful, 404 means not found, 401 means unauthorized
+            data = response.json()
+            for item in data.get('items', []):
+                volume_info = item.get('volumeInfo', [])
+                title = volume_info.get('title', 'N/A')
+                publisher = volume_info.get('publisher','N/A')
+                published_date = volume_info.get('publishedDate', 'N/A')
+                author = volume_info.get('authors', ['N/A'])
+                rating = volume_info.get('averageRating', ['N/A'])
+                image_links = volume_info.get('imageLinks',{})
+                image = image_links.get('thumbnail') if 'thumbnail' in image_links else 'N/A'
+
+                print(title)
+                print(publisher)
+                print(published_date)
+                print(author)
+                print(rating)
+                print(image)
+
+                fetch_information(title, image, published_date, rating)
+
+                if check_var.get() or check_var2.get():
+                    frame11.place(x=160, y=600)
+                    frame22.place(x=360, y=600)
+                    frame33.place(x=560, y=600)
+                    frame44.place(x=760, y=600)
+                    frame55.place(x=960, y=600)
+
+                else:
+                    frame11.place_forget()
+                    frame22.place_forget()
+                    frame33.place_forget()
+                    frame44.place_forget()
+                    frame55.place_forget()
+
+        else:
+            print("Failed to fetch the data from Google Books API.")
+            messagebox.showinfo("Info", "Failed to fetch the data from Google Books API.")
+
+def show_menu(event):
+    # Display the menu at the mouse position
+    menu.post(event.x_root, event.y_root)
+
 root = Tk()
 root.title("Book Recommendation System")
 root.geometry("1250x700")
 root. config(bg="#111119")
+root.resizable(False, False)
 
 #################################################################################################################
 
@@ -39,17 +123,27 @@ search_entry.place(x=415, y=172)
 
 # search button
 recommend_button_image = PhotoImage(file="Images/Search.png")
-recommend_button = Button(root, image=recommend_button_image, bg="#0099ff", bd=0, activebackground="#252532", cursor="hand1")
+recommend_button = Button(root, image=recommend_button_image, bg="#0099ff", bd=0, activebackground="#252532", cursor="hand1", command=search)
 recommend_button.place(x=860, y=169)
 
 # setting button
 Setting_image = PhotoImage(file="Images/setting.png")
 setting = Button(root, image=Setting_image, bd=0, cursor="hand2", activebackground="#0099ff", bg="#0099ff")
 setting.place(x=1050, y=175)
+setting.bind('<Button-1>', show_menu)
+
+# Menu for the search button
+menu = Menu(root, tearoff=0)
+
+check_var = BooleanVar()
+menu.add_checkbutton(label="Publish Date", variable=check_var, command=lambda: print(f"Check option is {'checked' if check_var.get() else 'unchecked'}"))
+
+check_var2 = BooleanVar()
+menu.add_checkbutton(label="Rating", variable=check_var2, command=lambda: print(f"Rating check option is {'checked' if check_var.get() else 'unchecked'}"))
 
 # logout button
 Logout_image = PhotoImage(file="Images/logout.png")
-Button(root, image=Logout_image, bg="#0099ff", cursor="hand1", command=lambda: root.destroy()).place(x=1150, y=20)
+Button(root, image=Logout_image, bg="#0099ff", cursor="hand1", command=lambda: root.destroy()).place(x=1200, y=20)
 
 ##################################################################################################################
 
@@ -89,14 +183,9 @@ frame22 = Frame(root, width=150, height=50, bg="#e6e6e6")
 frame33 = Frame(root, width=150, height=50, bg="#e6e6e6")
 frame44 = Frame(root, width=150, height=50, bg="#e6e6e6")
 frame55 = Frame(root, width=150, height=50, bg="#e6e6e6")
-frame11.place(x=160, y=600)
-frame22.place(x=360, y=600)
-frame33.place(x=560, y=600)
-frame44.place(x=760, y=600)
-frame55.place(x=960, y=600)
 
 # published date
-text2 = {'a11':Label(frame11, text="date", font=('arial', 10), bg="#e6e6e6"), 'a22':Label(frame22, text="date", font=('arial', 10), bg="#e6e6e6"), 'a33':Label(frame33, text="date", font=('arial', 10), bg="#e6e6e6"), 'a44':Label(frame44, text="date", font=('arial', 10), bg="#e6e6e6"), 'a55':Label(frame55, text="date", font=('arial', 10), bg="#e6e6e6")}
+text2 = {'a11':Label(frame11, text="date", font=('arial', 10), fg="red", bg="#e6e6e6"), 'a22':Label(frame22, text="date", font=('arial', 10), fg="red", bg="#e6e6e6"), 'a33':Label(frame33, text="date", font=('arial', 10), fg="red", bg="#e6e6e6"), 'a44':Label(frame44, text="date", font=('arial', 10), fg="red", bg="#e6e6e6"), 'a55':Label(frame55, text="date", font=('arial', 10), fg="red", bg="#e6e6e6")}
 text2['a11'].place(x=10, y=4)
 text2['a22'].place(x=10, y=4)
 text2['a33'].place(x=10, y=4)
